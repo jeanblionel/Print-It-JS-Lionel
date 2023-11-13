9d241b096a4d4cec850ae838a0e4115398f1d6fd
jeanblionel <jeanblionel@gmail.com>
2023-11-13 21:01:45 +0100

    Fonctionnalité du caroussel page js

--- assets/script.js
+++ assets/script.js
@@ -17,6 +17,7 @@ const slides = [
 	}
 ]
 
+//Test d'un pointer sur les flèches du slider
 /*const cursorPointer = document.querySelectorAll('.arrow')
 console.log(cursorPointer);
 cursorPointer.addEventListener('mouseover', (e)=>{
@@ -25,12 +26,15 @@ cursorPointer.addEventListener('mouseover', (e)=>{
 
 const arrowLeft = document.querySelector('#banner .arrow_left');
 arrowLeft.addEventListener('click', (e) => {
-  //  console.log('gauche');
+  moveSlide(-1);
+  dotList[numero + 1].classList.remove('dot_selected');
 })
 
 const arrowRight = document.querySelector('#banner .arrow_right');
 arrowRight.addEventListener('click', (e) => {
-   // console.log('droite');
+    console.log('droite');
+    moveSlide(1);
+    dotList[numero - 1].classList.remove('dot_selected');
 })
 
 const dotsSlider = document.querySelector('div .dots');
@@ -44,3 +48,45 @@ for (let slide of slides) {
 
 const dotList = document.querySelectorAll('.dots .dot');
  dotList[0].classList.add('dot_selected');
+
+//Mise en place du caroussel infini
+const imgSlide = document.querySelector('.banner-img')
+const tagLine = document.querySelector('p');
+
+
+let numero = 0;
+const moveSlide = (sens) => {
+    numero = (numero + sens + slides.length) % slides.length;
+    updateSlide();
+};
+
+
+
+const updateSlide = ()=>{
+    imgSlide.src ="./assets/images/slideshow/" + slides[numero].image;
+    tagLine.innerHTML = slides[numero].tagLine;
+    updateDots()
+}
+
+
+
+//Actualisation des bullet points quand on change d'image
+const updateDots = ()=> {
+    dotList.forEach((dot, index)=>{
+        if (index === numero) {
+            dot.classList.add('dot_selected')
+        } else {
+            dot.classList.remove('dot_selected')
+        }
+    });
+};
+
+
+//Utilisation des bullet points pour la navigation
+dotList.forEach((dot, index)=> {
+    dot.addEventListener('click', ()=>{
+        numero = index;
+        updateSlide()
+    })
+})
+ 
